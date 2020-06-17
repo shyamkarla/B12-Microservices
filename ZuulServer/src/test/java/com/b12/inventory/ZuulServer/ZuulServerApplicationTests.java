80df7a0a07d9e355e1b24a9fa80cd137a14fe470
shyamkarla <shyamkarla@gmail.com>
2020-06-17 19:28:23 +0530

    Add files via upload
    Zuul Server initial commit

---
+++ ZuulServer/src/test/java/com/b12/inventory/ZuulServer/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b12.inventory.ZuulServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
