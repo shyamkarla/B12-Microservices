e607fe19b3e430dd837b55e6594db4b7bc135cf5
shyamkarla <shyamkarla@gmail.com>
2020-06-17 19:29:33 +0530

    Configuration server initial commit

---
+++ ConfigurationServer/src/test/java/com/b12/config/ConfigurationServer/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b12.config.ConfigurationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
