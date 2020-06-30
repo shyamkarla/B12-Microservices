a6ed8443761b4c63c01961b79196af9492a62192
shyam <shyamkarla@gmail.com>
2020-06-30 17:36:08 +0530

    Service impl

---
+++ InventoryService/src/test/java/com/b12/inventory/inventoryService/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b12.inventory.inventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
